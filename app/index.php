20e51f73fa219333e203d2ce1e3012ab5c154075
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2025-03-20 01:48:45 +0000

    Aula 19/03/2025

--- app/index.php
+++ app/index.php
@@ -1,8 +1,46 @@
 <?php
-// require './vendor/autoload.php';
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-// use Application\DBConnection\MySQLConnection;
+use Slim\Views\PhpRenderer;
 
-// $db = new MySQLConnection();
+require __DIR__ . '/vendor/autoload.php';
 
-echo phpinfo();
+$app = AppFactory::create();
+
+$app->get(
+    '/',
+    function(
+        Request $request,
+        Response $response,
+        $args
+    ) {
+        $response->getBody()->write("Olá Mundo!!!");
+        return $response;
+});
+
+$app->get(
+    '/mensagem/{nome}',
+    function(
+        Request $request,
+        Response $response,
+        $args
+    ) {
+        $response->getBody()->write("Boa Noite " . $args['nome']);
+        return $response;
+});
+
+$app->get(
+    '/hello',
+    function(
+        Request $request,
+        Response $response,
+        $args
+    ) {
+        $renderer = new PhpRenderer(__DIR__ . '/templates');
+        $msg = 'Nova Mensagem';
+        return $renderer->render($response, 'hello.php', ['mensagem' => $msg]);
+});
+
+$app->run();
--- assets/scripts/enable-pdo_mysql.sh
+++ assets/scripts/enable-pdo_mysql.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker exec php sh -c 'exec docker-php-ext-install pdo_mysql' &&
     docker exec php sh -c 'docker-php-ext-enable pdo_mysql' &&
+    docker exec php a2enmod rewrite &&
     docker exec php sh -c 'apachectl restart'
